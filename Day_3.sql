335076d8cdabb201aab8c013f8c0dab81488df9e
Pragati P Swain <pragati.pratishruti@gmail.com>
2025-11-06 10:05:15 +0530

    Add files via upload

---
+++ Day_3.sql
@@ -0,0 +1,28 @@
+### Practice Questions:
+-- 1. List all patients sorted by age in descending order.
+select * from patients order by age desc;
+
+-- 2. Show all services_weekly data sorted by week number ascending and patients_request descending.
+select * from services_weekly order by week, patients_request desc;
+
+-- 3. Display staff members sorted alphabetically by their names.
+select staff_name from staff order by staff_name;
+
+### Daily Challenge:
+-- Retrieve the top 5 weeks with the highest patient refusals across all services, showing week, service, 
+-- patients_refused, and patients_request. Sort by patients_refused in descending order.
+with cte as(
+	select
+		week,
+        service, 
+		patients_refused, 
+		dense_rank() over(partition by service order by patients_refused desc, week) as rnk,
+        patients_request
+	from services_weekly
+)
+select distinct
+	week,
+	service, 
+	patients_refused,
+	patients_request
+from cte where rnk < 6;
