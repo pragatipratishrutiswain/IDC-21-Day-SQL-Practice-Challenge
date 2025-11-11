cb398123f35589504bc6e08a59a7c8f919a7b5eb
Pragati P Swain <pragati.pratishruti@gmail.com>
2025-11-11 11:51:59 +0530

    Update Day_3.sql

--- Day_3.sql
+++ Day_3.sql
@@ -11,9 +11,6 @@ select * from staff order by staff_name;
 ### Daily Challenge:
 -- Retrieve the top 5 weeks with the highest patient refusals across all services, showing week, service, 
 -- patients_refused, and patients_request. Sort by patients_refused in descending order.
-# Daily Challenge:
--- Retrieve the top 5 weeks with the highest patient refusals across all services, showing week, service, 
--- patients_refused, and patients_request. Sort by patients_refused in descending order.
 select distinct
 	week,
 	service, 
