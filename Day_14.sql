2a749dc63f804e78f0910d024c26759cabe62f58
Pragati P Swain <pragati.pratishruti@gmail.com>
2025-11-18 23:56:27 +0530

    Add ORDER BY clause to staff schedule query

--- Day_14.sql
+++ Day_14.sql
@@ -43,7 +43,8 @@ select
     SUM(COALESCE(ss.present, 0)) AS weeks_present
 FROM staff s
 LEFT JOIN staff_schedule ss ON s.staff_id = ss.staff_id
-GROUP BY s.staff_id, s.staff_name, s.role, s.service;
+GROUP BY s.staff_id, s.staff_name, s.role, s.service
+ORDER BY weeks_present DESC;
 
 
 
